0e1860dcf21a6ab94e92f650eb7b23554793357b
Donald Whitely <45896739+dswhitely1@users.noreply.github.com>
2020-07-18 11:02:02 -0400

    Testing Components as they're being build

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import 'index.scss';
-const App = () => <h1>Hello Emergency Electric</h1>;
+import { StylesProvider } from '@material-ui/core';
+import Container from 'components/Container/container.component';
+import GridRow from 'components/Grid/gridRow.component';
+import GridColumn from 'components/Grid/gridColumn.component';
+import Button from 'components/CustomButtons/button.component';
+
+const App = () => (
+  <StylesProvider injectFirst>
+    <Container maxWidth="lg">
+      <GridRow>
+        <GridColumn xs={12} sm={6} md={4}>
+          <h1>Hello Emergency Electric</h1>
+          <Button size="sm" color="primary">
+            Hello World
+          </Button>
+        </GridColumn>
+      </GridRow>
+    </Container>
+  </StylesProvider>
+);
 
 render(<App />, document.getElementById('root'));
