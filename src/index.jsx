edb4a951c7f3f2939188e736e80bb869faa21738
Donald Whitely <45896739+dswhitely1@users.noreply.github.com>
2020-07-18 14:59:33 -0400

    Testing Card

--- src/index.jsx
+++ src/index.jsx
@@ -6,19 +6,16 @@ import Container from 'components/Container/container.component';
 import GridRow from 'components/Grid/gridRow.component';
 import GridColumn from 'components/Grid/gridColumn.component';
 import Button from 'components/CustomButtons/button.component';
+import Card from 'components/Cards/Card/card.component';
 
 const App = () => (
   <StylesProvider injectFirst>
-    <Container maxWidth="lg">
-      <GridRow>
-        <GridColumn xs={12} sm={6} md={4}>
-          <h1>Hello Emergency Electric</h1>
-          <Button size="sm" color="primary">
-            Hello World
-          </Button>
-        </GridColumn>
-      </GridRow>
-    </Container>
+    <Card>
+      <h1>Hello Emergency Electric</h1>
+      <Button size="sm" color="primary">
+        Hello World
+      </Button>
+    </Card>
   </StylesProvider>
 );
 
