e530c58969608d8712bf373e79100d84abfb618a
Donald Whitely <45896739+dswhitely1@users.noreply.github.com>
2020-07-18 18:32:15 -0400

    Landing Page Layout

---
+++ src/layouts/LandingPage/landingPage.component.jsx
@@ -0,0 +1,18 @@
+import React, { createRef } from 'react';
+import './landingPage.styles.scss';
+
+function LandingPage({ ...rest }) {
+  return (
+    <div>
+      <div>Future Home of the Landing Page Navbar</div>
+      <div className="landing-page-wrapper">
+        <div className="landing-page-full-page">
+          <p>Routes PlaceHolder</p>
+          <p>Footer PlaceHolder</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
