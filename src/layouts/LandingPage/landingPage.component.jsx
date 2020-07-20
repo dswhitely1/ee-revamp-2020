827c37f9d6f57b0d3e2f4484bef9e3ef8f4f1830
Donald Whitely <45896739+dswhitely1@users.noreply.github.com>
2020-07-20 12:58:44 -0400

    Added NavBar to Landing Page

--- src/layouts/LandingPage/landingPage.component.jsx
+++ src/layouts/LandingPage/landingPage.component.jsx
@@ -1,10 +1,11 @@
 import React, { createRef } from 'react';
 import './landingPage.styles.scss';
+import LandingPageNavBar from 'components/NavBars/LandingPage/landingPageNavBar.component';
 
 function LandingPage({ ...rest }) {
   return (
     <div>
-      <div>Future Home of the Landing Page Navbar</div>
+      <LandingPageNavBar color="primary" {...rest} />
       <div className="landing-page-wrapper">
         <div className="landing-page-full-page">
           <p>Routes PlaceHolder</p>
