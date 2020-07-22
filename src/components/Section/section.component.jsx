163dd65be3bceac556ce8eca82d294e12e5ddde4
Donald Whitely <45896739+dswhitely1@users.noreply.github.com>
2020-07-22 08:22:51 -0400

    Moved Background Image To Material UI Component

--- src/components/Section/section.component.jsx
+++ src/components/Section/section.component.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import './section.styles.scss';
+import header from 'assets/header.jpg';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  background: {
+    [theme.breakpoints.down('sm')]: {
+      background: `linear-gradient(90deg, rgba(255,255,255,0.1) 0, rgba(255,255,255,0.1) 100%), url(${header})`,
+      backgroundPosition: 'center center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+    },
+  },
+}));
 
 function Section({ children, className, header, inverted, ...rest }) {
+  const classes = useStyles();
   const sectionClasses = cx({
     section: true,
     'section-header': header,
+    [classes.background]: header,
     'section-inverted': inverted,
     [className]: className !== undefined,
   });
