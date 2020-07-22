59791462c615f951b4a9cbdbb98a64dc067a02d4
Donald Whitely <45896739+dswhitely1@users.noreply.github.com>
2020-07-22 07:34:05 -0400

    Landing Page Layout Finished

--- src/layouts/LandingPage/landingPage.component.jsx
+++ src/layouts/LandingPage/landingPage.component.jsx
@@ -2,14 +2,26 @@ import React, { createRef } from 'react';
 import './landingPage.styles.scss';
 import LandingPageNavBar from 'components/NavBars/LandingPage/landingPageNavBar.component';
 import Footer from 'components/Footer/footer.component';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import LandingPageHome from 'views/landingPage.component';
+import header from 'assets/header.jpg';
 
 function LandingPage({ ...rest }) {
   return (
     <div>
       <LandingPageNavBar color="primary" {...rest} />
-      <div className="landing-page-wrapper">
+      <div
+        className="landing-page-wrapper"
+        style={{ backgroundImage: `url(${header})` }}
+      >
         <div className="landing-page-full-page">
-          <p>Routes PlaceHolder</p>
+          <Switch>
+            <Route
+              to="/home/emergency-electric-inc"
+              component={LandingPageHome}
+            />
+            <Redirect from="/home" to="/home/emergency-electric-inc" />
+          </Switch>
           <Footer />
         </div>
       </div>
